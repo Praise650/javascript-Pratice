8ea797256f83c47f06e76617b4a8dae49cc49036
Praise Afuwape <ayokanmi1275@gmail.com>
2023-11-18 23:46:41 +0100

    added basic structure

--- interactive story/index.js
+++ interactive story/index.js
@@ -1,3 +1,30 @@
-<script>
-    document.write("Hello! World.")
-</script>
+// document.getElementById('demo').innerHTML="Hello";
+
+let person = {
+    name: "Praise",
+    age: 23,
+};
+
+allowAccess(person.age);
+
+function allowAccess(age) {
+    if (age < 18) {
+        console.log('You are not granted access');
+    } else {
+        console.log('Access granted');
+    }
+}
+
+const maxLenght = 10;
+let selectedColor = ['red','blue'];
+for(let index = 0; index<maxLenght; index++){
+    console.log(index);
+    selectedColor.push('gre')
+    if(index < 2){
+        // return;
+    } else{
+        selectedColor[index ] = 'green' + index;
+    }
+}
+console.log(selectedColor);
+
