5ca80da7a376bd96a5000ff1bb9bda6af6aeea5f
Praise Afuwape <ayokanmi1275@gmail.com>
2023-11-19 21:01:59 +0100

    added dynamic interactions

--- interactive story/index.js
+++ interactive story/index.js
@@ -1,4 +1,22 @@
 // document.getElementById('demo').innerHTML="Hello";
+// const sayHello = document.querySelector('[onclick="start()"]');
+
+let showNextSection = false;
+
+function showStory(text) {
+    const story = document.getElementById('story');
+    const newParagraph = document.createElement('span');
+    newParagraph.textContent = text;
+    story.appendChild(newParagraph);
+}
+
+function start() {
+    const buttonElement = document.querySelector('.get-started');
+    showStory('You choose to explore the misterious forest. ');
+    console.log("Say Hello");
+    showNextSection = true;
+    showStory("You chose to explore the mysterious forest. ");
+}
 
 let person = {
     name: "Praise",
@@ -16,14 +34,14 @@ function allowAccess(age) {
 }
 
 const maxLenght = 10;
-let selectedColor = ['red','blue'];
-for(let index = 0; index<maxLenght; index++){
+let selectedColor = ['red', 'blue'];
+for (let index = 0; index < maxLenght; index++) {
     console.log(index);
     selectedColor.push('gre')
-    if(index < 2){
+    if (index < 2) {
         // return;
-    } else{
-        selectedColor[index ] = 'green' + index;
+    } else {
+        selectedColor[index] = 'green' + index;
     }
 }
 console.log(selectedColor);
