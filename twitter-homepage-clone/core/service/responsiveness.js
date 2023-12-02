2b98c4be9d5bd8736c38d83875a0ab5ba363b841
Praise Afuwape <ayokanmi1275@gmail.com>
2023-12-02 23:36:08 +0100

    responsiveness with javascript

--- twitter-homepage-clone/core/service/responsiveness.js
+++ twitter-homepage-clone/core/service/responsiveness.js
@@ -1,26 +1,46 @@
 const breakpoints = {
-    670: 'path/to/your-670.css',
-    1024: 'path/to/your-1024.css',
-    // Add more breakpoints as needed
+    670: 'index-mobile.css',
+    1024: 'index.css',
   };
   
   function loadCSSForBreakpoints() {
     const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
   
-    for (const breakpoint in breakpoints) {
-      if (viewportWidth >= parseInt(breakpoint)) {
-        loadCSS(breakpoints[breakpoint]);
+    // let cssToLoad = '';
+  
+    // Find the appropriate CSS to load based on the viewport width
+    // for (const breakpoint in breakpoints) {
+    //   if (viewportWidth <= parseInt(breakpoint)) {
+    //     cssToLoad = breakpoints[breakpoint];
+    //     break;
+    //   }
+    // }
+
+    if (viewportWidth <= 670) {
+        const cssToLoad = breakpoints[670];
+        loadCSS(cssToLoad);
+      } else{
+        const cssToLoad = breakpoints[1024];
+        loadCSS(cssToLoad);
       }
-    }
+  
+    // Load the CSS file if a valid one is found
+    // if (cssToLoad) {
+    //   loadCSS(cssToLoad);
+    // }
   }
   
   function loadCSS(cssPath) {
     const cssLink = document.createElement('link');
     cssLink.rel = 'stylesheet';
+    cssLink.type ='text/css';
     cssLink.href = cssPath;
     document.head.appendChild(cssLink);
   }
   
-  window.addEventListener('resize', loadCSSForBreakpoints);
+  // Load CSS initially on page load
   window.addEventListener('DOMContentLoaded', loadCSSForBreakpoints);
+  
+  // Load CSS when the window is resized
+  window.addEventListener('resize', loadCSSForBreakpoints);
   
